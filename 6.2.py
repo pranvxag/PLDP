9874f3b655a833c94895a40135b704b5b22d8c7a
Pranav Agale <agalepranav2015@gmail.com>
2026-03-12 17:09:07 +0530

    Create 6.2.py

---
+++ 6.2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+b = np.array([5, 6, 7, 8])
+
+result = a * b
+
+print("Array 1:", a)
+print("Array 2:", b)
+print("Element-wise multiplication:", result)
